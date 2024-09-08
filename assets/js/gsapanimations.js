179f3d53b859abda235ef4e9b409b8976180995e
aniket-digiweave <aniket@digiweave.in>
2024-09-08 19:11:19 +0530

    added gsap animations

---
+++ assets/js/gsapanimations.js
@@ -0,0 +1,41 @@
+gsap.from(".animated-image", {
+  opacity: 0,
+  x: -200,
+  //   duration:2,
+  rotate: -10,
+  scrollTrigger: {
+    trigger: ".about-image-container",
+    scroller: "body",
+    start: "top 50%",
+    end: "top 10%",
+    scrub: true,
+  },
+});
+const tl = gsap.timeline();
+
+tl.from(".video-container video", {
+  opacity: 0.5,
+  scale: 0.5,
+  duration: 1,
+  ease: "power1.out",
+})
+  .from(
+    ".video-text-content",
+    {
+      opacity: 0,
+      y: 100,
+      duration: 2,
+      ease: "power1.out",
+    },
+    "-=0.5"
+  )
+  .from(
+    ".navigation-bar",
+    {
+      opacity: 0,
+      scale: 0.7,
+      duration: 1,
+      ease: "power1.out",
+    },
+    "-=0.5"
+  );
