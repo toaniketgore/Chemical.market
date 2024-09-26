90ead908058e0da89d9e8c90b31045634f7ac38d
aniket-digiweave <aniket@digiweave.in>
2024-09-26 17:06:31 +0530

    added animation to self careproduct page

---
+++ assets/js/productcategoryanimgsap.js
@@ -0,0 +1,30 @@
+// let tl = gsap.timeline();
+
+gsap.from(".filter-desc", {
+  opacity: 0,
+  x: -150,
+  //   ease: "power4.inOut",
+  // stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".belowfiltersection",
+    start: "top 80%",
+    end: "top 50%",
+    markers: true,
+    scrub: true,
+  },
+});
+
+gsap.from(".grid-item img", {
+  scale: 1.3,
+  opacity: 0,
+  ease: "elastic.out(1, 0.3)",
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".belowfiltersection",
+    start: "top 80%",
+    end: "top 50%",
+    markers: true,
+    scrub: true,
+    toggleActions: "restart none none reverse",
+  },
+});
