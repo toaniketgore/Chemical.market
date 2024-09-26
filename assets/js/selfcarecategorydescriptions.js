b891691642a1375121b295ffe06bdf3567aae5c5
aniket-digiweave <aniket@digiweave.in>
2024-09-26 12:10:57 +0530

    set the images

--- assets/js/selfcarecategorydescriptions.js
+++ assets/js/selfcarecategorydescriptions.js
@@ -2,20 +2,49 @@ document.addEventListener("DOMContentLoaded", function () {
   var gridItems = document.querySelectorAll(".grid-item");
   var filterButtons = document.querySelectorAll(".filters button");
   var descriptions = {
+    hydrolyzedProteins:
+      "These proteins are used to strengthen and repair damaged hair and skin by providing essential amino acids that enhance moisture retention and improve overall texture. Their ability to penetrate and nourish at a cellular level makes them a key ingredient in formulations aimed at restoring vitality and resilience.",
+
+    beautyPeptides:
+      "These peptides are used to stimulate collagen production, enhance skin elasticity, and reduce the appearance of fine lines and wrinkles. Their ability to target specific skin concerns at a cellular level makes them essential in formulations designed to promote a youthful, radiant complexion.",
+
     moisturizers:
       "These ingredients are essential for maintaining skin hydration, improving texture, and enhancing the skin's natural barrier. They also lock in moisture and prevent dryness to give a smooth and supple appearance.",
+
     surfactants:
-      "These chemical compounds are essential for their cleansing and emulsifying properties. They help remove dirt, oil, and impurities from the skin and hair by reducing surface tension, enabling effective cleaning and foaming action",
+      "These chemical compounds are essential for their cleansing and emulsifying properties. They help remove dirt, oil, and impurities from the skin and hair by reducing surface tension, enabling effective cleaning and foaming action.",
+
     brighteners:
-      "These agents are used to reduce the appearance of hyperpigmentation, even out skin tone, and brighten the complexion. They work by inhibiting melanin production and promoting a more radiant, uniform skin appearance, making them popular in formulations",
-    cleansers:
-      "These chemical compounds are essential for their cleansing and emulsifying properties. They help remove dirt, oil, and impurities from the skin and hair by reducing surface tension, enabling effective cleaning and foaming action",
-    oils: "They provide deep hydration, support skin repair, and deliver essential fatty acids and antioxidants, making them ideal for formulating products that promote healthy, radiant skin and hair while aligning with eco-conscious consumer preferences",
+      "These agents are used to reduce the appearance of hyperpigmentation, even out skin tone, and brighten the complexion. They work by inhibiting melanin production and promoting a more radiant, uniform skin appearance, making them popular in formulations.",
+
+    inflammatory:
+      "These ingredients are crucial for soothing irritated skin, reducing redness, and calming inflammation.",
+
+    conditioningAgents:
+      "These agents are used to improve the texture and manageability of hair and skin. They enhance moisture retention, smoothness, and overall softness, making them essential for formulations aimed at nourishing and revitalizing hair and healthy skin.",
+
+    emollients:
+      "These cosmetic preparations soften the skin by providing deep hydration and creating a protective barrier to prevent moisture loss.",
+
+    exoticButters:
+      "These butters are valued for their rich, nourishing properties. They provide intense hydration, improve skin elasticity, and offer a luxurious texture, making them ideal for enhancing the sensory appeal of skincare and haircare products.",
+
+    emulsifiers:
+      "They ensure consistent texture and performance in products such as creams and lotions, preventing separation and enhancing the overall effectiveness and application of the products.",
+
+    beautyOils:
+      "They provide deep hydration, support skin repair, and deliver essential fatty acids and antioxidants, making them ideal for formulating products that promote healthy, radiant skin and hair.",
+
     default: "", // For "All" filter, no description
   };
 
-  // Show all items on page load
-  filterItems("*");
+  // Automatically display the content for the active filter on page load
+  var activeButton = document.querySelector(".filters button.filter-active");
+  var defaultFilter = activeButton.getAttribute("data-filter");
+
+  // Show the grid items and description for the default active filter
+  filterItems(defaultFilter);
+  updateDescription(defaultFilter);
 
   // Add event listener to filter buttons
   filterButtons.forEach(function (button) {
@@ -32,12 +61,11 @@ document.addEventListener("DOMContentLoaded", function () {
       filterItems(filterValue);
 
       // Update the description
-      var descKey = filterValue.replace(".", ""); // Remove "." from filter class
-      var description = descriptions[descKey] || descriptions["default"];
-      document.querySelector(".desc-content").textContent = description;
+      updateDescription(filterValue);
     });
   });
 
+  // Function to show/hide grid items based on filter
   function filterItems(filterValue) {
     gridItems.forEach(function (item) {
       if (
@@ -51,5 +79,12 @@ document.addEventListener("DOMContentLoaded", function () {
         item.style.opacity = "0";
       }
     });
+  }
+
+  // Function to update the description based on the selected filter
+  function updateDescription(filterValue) {
+    var descKey = filterValue.replace(".", ""); // Remove "." from filter class
+    var description = descriptions[descKey] || descriptions["default"];
+    document.querySelector(".desc-content").textContent = description;
   }
 });
