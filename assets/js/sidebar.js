70c75d4c6d18951ebe92dbd76452b5f667e3a23d
aniket-digiweave <aniket@digiweave.in>
2024-10-17 16:28:23 +0530

    Mionr changes

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,13 +1,35 @@
-const sidebarProductsToggle = document.getElementById(
-  "sidebar-products-toggle"
-);
-const sidebarDropdown = document.getElementById("sidebar-dropdown");
-const sidebarBtn = document.querySelector(".sidebar-btn");
-sidebarProductsToggle.addEventListener("click", function () {
-  if (sidebarDropdown.style.display === "flex") {
-    sidebarDropdown.style.display = "none";
-    sidebarBtn.style.ZIndex = "0";
-  } else {
-    sidebarDropdown.style.display = "flex";
-  }
-});
+// document.addEventListener("DOMContentLoaded", function () {
+  const sidebar = document.getElementById("sidebar");
+  const hamburger = document.getElementById("hamburger");
+  const dropdownToggle = document.querySelector("#sidebar .dropdown > a");
+  const submenu = document.querySelector("#sidebar .submenu");
+
+  hamburger.addEventListener("click", function () {
+    sidebar.classList.toggle("active");
+    this.classList.toggle("active");
+  });
+
+  dropdownToggle.addEventListener("click", function (e) {
+    e.preventDefault();
+    submenu.style.display =
+      submenu.style.display === "block" ? "none" : "block";
+  });
+
+  let lastScrollTop = 0;
+  let isScrolling;
+
+  window.addEventListener(
+    "scroll",
+    function () {
+      hamburger.style.opacity = "0";
+      clearTimeout(isScrolling);
+
+      isScrolling = setTimeout(function () {
+        hamburger.style.opacity = "1";
+      }, 250);
+
+      lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    },
+    false
+  );
+// });
