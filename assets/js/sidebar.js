4ee9c11b46b6462bda5c146ed84a9cc323c33ccf
aniket-digiweave <aniket@digiweave.in>
2024-10-18 13:38:48 +0530

    new logo updated

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,35 +1,32 @@
-// document.addEventListener("DOMContentLoaded", function () {
-  const sidebar = document.getElementById("sidebar");
-  const hamburger = document.getElementById("hamburger");
-  const dropdownToggle = document.querySelector("#sidebar .dropdown > a");
-  const submenu = document.querySelector("#sidebar .submenu");
+const sidebar = document.getElementById("sidebar");
+const hamburger = document.getElementById("hamburger");
+const dropdownToggle = document.querySelector("#sidebar .dropdown > a");
+const submenu = document.querySelector("#sidebar .submenu");
 
-  hamburger.addEventListener("click", function () {
-    sidebar.classList.toggle("active");
-    this.classList.toggle("active");
-  });
+hamburger.addEventListener("click", function () {
+  sidebar.classList.toggle("active");
+  this.classList.toggle("active");
+});
 
-  dropdownToggle.addEventListener("click", function (e) {
-    e.preventDefault();
-    submenu.style.display =
-      submenu.style.display === "block" ? "none" : "block";
-  });
+dropdownToggle.addEventListener("click", function (e) {
+  e.preventDefault();
+  submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+});
 
-  let lastScrollTop = 0;
-  let isScrolling;
+let lastScrollTop = 0;
+let isScrolling;
 
-  window.addEventListener(
-    "scroll",
-    function () {
-      hamburger.style.opacity = "0";
-      clearTimeout(isScrolling);
+window.addEventListener(
+  "scroll",
+  function () {
+    hamburger.style.opacity = "0";
+    clearTimeout(isScrolling);
 
-      isScrolling = setTimeout(function () {
-        hamburger.style.opacity = "1";
-      }, 250);
+    isScrolling = setTimeout(function () {
+      hamburger.style.opacity = "1";
+    }, 250);
 
-      lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    },
-    false
-  );
-// });
+    lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  },
+  false
+);
